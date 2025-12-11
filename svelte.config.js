2050923e7072bdeae089ea1bbf72f7bce2bdc1f1
NullableNinja <thomasebryantiii@users.noreply.github.com>
2025-12-10 20:32:57 -0600

    Fix base path for GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,18 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		})
-	}
+kit: {
+adapter: adapter({
+pages: 'build',
+assets: 'build',
+fallback: '404.html',
+precompress: false,
+strict: true
+}),
+paths: {
+base: process.env.NODE_ENV === 'production' ? '/karate-connect' : ''
+}
+}
 };
 
 export default config;
